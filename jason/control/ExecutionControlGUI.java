dbe89288f26d784afc1e00310e686d320520e13e
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2005-08-12 20:45:36 +0000

    change informAgs method name

--- jason/control/ExecutionControlGUI.java
+++ jason/control/ExecutionControlGUI.java
@@ -1,3 +1,34 @@
+//----------------------------------------------------------------------------
+// Copyright (C) 2003  Rafael H. Bordini, Jomi F. Hubner, et al.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// To contact the authors:
+// http://www.dur.ac.uk/r.bordini
+// http://www.inf.furb.br/~jomi
+//
+// CVS information:
+//   $Date$
+//   $Revision$
+//   $Log$
+//   Revision 1.5  2005/08/12 20:45:36  jomifred
+//   change informAgs method name
+//
+
+//----------------------------------------------------------------------------
+
 package jason.control;
 
 import java.awt.BorderLayout;
@@ -64,7 +95,7 @@ public class ExecutionControlGUI extends ExecutionControl {
 		jBtStep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				jBtStep.setEnabled(false);
-				fJasonControl.informAllAgToPerformCycle();
+				fJasonControl.informAllAgsToPerformCycle();
 			}
 		});
 
@@ -75,7 +106,7 @@ public class ExecutionControlGUI extends ExecutionControl {
 			public void actionPerformed(ActionEvent e) {
 				jBtRun.setEnabled(false);
 				inRunMode = true;
-				fJasonControl.informAllAgToPerformCycle();
+				fJasonControl.informAllAgsToPerformCycle();
 			}
 		});
 
@@ -200,7 +231,7 @@ public class ExecutionControlGUI extends ExecutionControl {
 	/** called when all agents have finished the current cycle */
 	protected void allAgsFinished() {
 		if (inRunMode) {
-			fJasonControl.informAllAgToPerformCycle();
+			fJasonControl.informAllAgsToPerformCycle();
 		} else {
 			inspectAgent(currentAg);
 			jBtStep.setEnabled(true);
