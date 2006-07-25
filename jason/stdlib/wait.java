ba26e85af6b7f386ce30cd9daa16e4744bdd8a0b
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2006-07-25 02:13:49 +0000

    .wait(1000) put the current intention in suspend state instead of all the agent

--- jason/stdlib/wait.java
+++ jason/stdlib/wait.java
@@ -40,126 +40,136 @@ import java.util.logging.Level;
 public class wait implements InternalAction {
 
     /**
-	 * args[0] is either a number or a string, if number it is the time (in ms), 
-	 * if string it is the trigger event to be waited. this second use also receive
-     * the timeout (in ms) as parameter.
-	 * 
-	 * E.g.: .wait(1000)             // waits 1 second
-	 *       .wait("+!t(50)")        // waits the event +!t(50)
-     *       .wait("+!t(50)", 2000)  // waits the event +!t(50) for 2 seconds
-	 * 
-	 */
-	public boolean execute(final TransitionSystem ts, Unifier un, Term[] args)	throws Exception {
-		if (args[0].isNumeric()) {
-			// time in mile seconds
-			NumberTerm time = (NumberTerm)args[0].clone();
-			un.apply(time);
-			try {
-				Thread.sleep((long)time.solve());
-			} catch (Exception e) {		}
-			return true;
-		} else if (args[0].isString()) {
-			// wait event
-			try {
-				StringTerm st = (StringTerm)args[0];
-				un.apply((Term)st);
-				Trigger te = Trigger.parseTrigger(st.getString());
-                
-                int timeout = -1;
+     * args[0] is either a number or a string, if number it is the time (in ms),
+     * if string it is the trigger event to be waited. this second use also
+     * receive the timeout (in ms) as parameter.
+     * 
+     * E.g.: .wait(1000) // waits 1 second 
+     * .wait("+!t(50)") // waits the event
+     * +!t(50) .wait("+!t(50)", 2000) // waits the event +!t(50) for 2 seconds
+     * 
+     */
+    public boolean execute(final TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        long timeout = -1;
+        Trigger te = null;
+        try {
+            if (args[0].isNumeric()) {
+                // time in mile seconds
+                NumberTerm time = (NumberTerm) args[0].clone();
+                un.apply(time);
+                timeout = (long) time.solve();
+                // Thread.sleep((long) time.solve());
+            } else if (args[0].isString()) {
+                // wait event
+                StringTerm st = (StringTerm) args[0];
+                un.apply((Term) st);
+                te = Trigger.parseTrigger(st.getString());
+
                 if (args.length == 2) {
-                    NumberTerm tot = (NumberTerm)args[1].clone();
-                    un.apply((Term)tot);
-                    timeout = (int)tot.solve();
+                    NumberTerm tot = (NumberTerm) args[1].clone();
+                    un.apply(tot);
+                    timeout = (long) tot.solve();
                 }
-                
-				new WaitEvent(te, un, ts, timeout).start();
-				return true;
-			} catch (Exception e) {
-				ts.getLogger().log(Level.SEVERE, "Error at .wait.",e);
-			}
-		}
-		return false;
-	}
 
-	class WaitEvent extends Thread implements CircumstanceListener {
-		Trigger te;
-		Unifier un; 
-		Intention si;
-		TransitionSystem ts;
-		Circumstance c;
-        boolean ok = false;
-        int timeout = -1;
-		
-		WaitEvent(Trigger te, Unifier un, TransitionSystem ts, int to) {
-			this.te = te;
-			this.un = un;
-			this.ts = ts;
-			c = ts.getC();
-			si = c.getSelectedIntention();
+            }
+        } catch (Exception e) {
+            ts.getLogger().log(Level.SEVERE, "Error at .wait.", e);
+            return false;
+        }
+        WaitEvent wet = new WaitEvent(te, un, ts, timeout);
+        wet.start();
+        return true;
+    }
+
+    private static int contTO = 1;
+
+    class WaitEvent extends Thread implements CircumstanceListener {
+        Trigger          te;
+        String           sTE; // an string version of TE
+        Unifier          un;
+        Intention        si;
+        TransitionSystem ts;
+        Circumstance     c;
+        boolean          ok      = false;
+        long             timeout = -1;
+        
+        WaitEvent(Trigger te, Unifier un, TransitionSystem ts, long to) {
+            this.te = te;
+            this.un = un;
+            this.ts = ts;
+            c = ts.getC();
+            si = c.getSelectedIntention();
             this.timeout = to;
-		}
 
-		public void run() {
-			try {
-				// register listener
-				c.addEventListener(this);		
-		
-				waitEvent();
+            // register listener
+            c.addEventListener(this);
+            
+            if (te != null) {
+                sTE = te.toString();
+            } else {
+                sTE = "time"+(contTO++);
+            }
+        }
 
-				// unregister (to not receive intentionAdded again)
-				c.removeEventListener(this);
+        public void run() {
+            try {
+                waitEvent();
 
-				// add SI again in C.I
-				c.addIntention(si);
-				c.getPendingIntentions().remove(te.toString());
-				
-			} catch (Exception e) {
-				ts.getLogger().log(Level.SEVERE, "Error at .wait thread",e);
-			}
-		}
-		
-		synchronized public boolean waitEvent() {
+                // unregister (to not receive intentionAdded again)
+                c.removeEventListener(this);
+
+                // add SI again in C.I if it was removed
+                if (!c.getIntentions().contains(si)) {
+                    c.addIntention(si);
+                    c.getPendingIntentions().remove(sTE);
+                }
+
+            } catch (Exception e) {
+                ts.getLogger().log(Level.SEVERE, "Error at .wait thread", e);
+            }
+        }
+
+        synchronized public boolean waitEvent() {
             long init = System.currentTimeMillis();
             long pass = 0;
             while (!ok) {
-                //ts.getLogger().info("* waiting "+te+" timeout="+timeout+" pass="+pass);
                 try {
                     if (timeout == -1) {
                         wait();
                     } else {
                         long to = timeout - pass;
-                        if (to <= 0) to = 100;
+                        if (to <= 0)
+                            to = 100;
                         wait(to);
-                        pass = System.currentTimeMillis()-init;
+                        pass = System.currentTimeMillis() - init;
                         if (pass >= timeout) {
                             break;
                         }
                     }
-    			} catch (Exception e) {
-    			    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
             return ok;
-		}		
+        }
 
-		synchronized public void eventAdded(Event e) {
-			if (un.unifies(te,e.getTrigger())) {
+        synchronized public void eventAdded(Event e) {
+            if (te != null && un.unifies(te, e.getTrigger())) {
                 ok = true;
-				notifyAll();
-			}
-		}
-		
-		public void intentionAdded(Intention i) {
-			// if the .wait intention where is being added in C.I, remove it
-			if (i == si) {
-			    if (c.removeIntention(si)) {
-                    c.getPendingIntentions().put(te.toString(), si);          
+                notifyAll();
+            }
+        }
+
+        public void intentionAdded(Intention i) {
+            // if the .wait intention where is being added in C.I, remove it
+            if (i == si) {
+                if (c.removeIntention(si)) {
+                    c.getPendingIntentions().put(sTE, si);
                 } else {
-                    ts.getLogger().warning("The following intentions sould be removed, but wasn't!"+si+"\nWait intention is"+i);
+                    ts.getLogger().warning("The following intentions sould be removed, but wasn't!" + si + "\nWait intention is" + i);
                 }
-			}
-		}
+            }
+        }
 
-	}	
+    }
 }
-
