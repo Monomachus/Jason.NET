05420e53d50670829b45cb2c146276c55beb5043
jomifred <jomifred@7ad00f97-b80e-0410-96fd-85ba2fd1e1ac>
2006-12-06 19:17:43 +0000

    add test

--- test/MAS2JParserTest.java
+++ test/MAS2JParserTest.java
@@ -15,19 +15,45 @@ public class MAS2JParserTest extends TestCase {
 	 
 	protected void setUp() throws Exception {
 		super.setUp();
-    	parser = new mas2j(new StringReader("MAS auctionCent {  infrastructure: Centralised environment: myEnv at \"x.edu\" executionControl: myController agents: ag1 [events=discard,intBels=newFocus,osfile=\"a/x.xml\"]; ag2 /home/agTest.asl agentClass mypkg.MyAgent #2; ag3 at \"x.edu\"; auctionner agentArchClass AuctionnerGUI;}"));
+        StringBuffer source = new StringBuffer("MAS auctionCent { ");
+        source.append("infrastructure: Centralised ");
+        source.append("environment: myEnv at \"x.edu\" ");
+        source.append("executionControl: myController ");
+        source.append("agents: ag1 [events=discard,intBels=newFocus,osfile=\"a/x.xml\"]; ag2 /home/agTest.asl agentClass mypkg.MyAgent #2; ag3 at \"x.edu\"; auctionner agentArchClass AuctionnerGUI;");
+        source.append("directives: md1=mypkg.DebugDirective; md2=mypkg.LogDirective;");
+        source.append("classpath: \"x.jar\"; \"../../\";");
+        source.append("}");
+    	parser = new mas2j(new StringReader(source.toString()));
 	}
+
+    public void testToString() {
+        boolean ok = true;
+        try {
+            MAS2JProject project = parser.mas();
+            System.out.println(project);
+            parser = new mas2j(new StringReader(project.toString()));
+            parser.mas();
+        } catch (Exception e) {
+            System.err.println("Error:"+e);
+            e.printStackTrace();
+            ok = false;
+        }
+        assertTrue(ok);
+    }
 
 	public void testParser() {
 		try {
 			MAS2JProject project = parser.mas();
 	    	project.setDirectory("/tmp");
-			//System.out.println(project);
 			
 			//project.writeXMLScript(System.out);
 			//project.writeScripts(true);
 			Map ag1Opt = project.getAg("ag1").options;
 			assertEquals(ag1Opt.size(),3);
+            
+            assertEquals(project.getDirectiveClasses().size(),2);
+            assertEquals(project.getDirectiveClasses().get("md2").toString(), "mypkg.LogDirective");
+            
 		} catch (Exception e) {
 			System.err.println("Error:"+e);
 			e.printStackTrace();
