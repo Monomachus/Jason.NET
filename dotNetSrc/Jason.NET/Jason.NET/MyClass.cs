4c2a4d7d637e81c207ed00a21caf38ee245d1a3f
Monomachus <monomachus85@yahoo.com>
2012-01-02 16:22:40 +0200

    Added SourceInfo class to .net solution

---
+++ dotNetSrc/Jason.NET/Jason.NET/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tpoperecinii
+ * Date: 1/2/2012
+ * Time: 4:00 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Jason.NET
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ dotNetSrc/Jason.NET/Jason.asSyntax/SourceInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Jason.asSyntax
+{
+	/**
+	 * Store information about the file source of some term (atom, literal, etc).
+	 * (immutable objects)
+	 */
+	public class SourceInfo : ISerializable
+	{
+
+		private readonly String      source;
+		private readonly int         beginSrcLine; // the line this literal appears in the source
+		private readonly int         endSrcLine;
+
+		public SourceInfo(String file, int beginLine) {
+			source       = file;
+			beginSrcLine = beginLine;
+			endSrcLine   = beginLine;
+		}
+		public SourceInfo(String file, int beginLine, int endLine) {
+			source       = file;
+			beginSrcLine = beginLine;
+			endSrcLine   = endLine;
+		}
+		public SourceInfo(SourceInfo o) {
+			if (o == null) {
+				source       = null;
+				beginSrcLine = 0;
+				endSrcLine   = 0;
+			} else {
+				source       = o.source;
+				beginSrcLine = o.beginSrcLine;
+				endSrcLine   = o.endSrcLine;
+			}
+		}
+
+		public SourceInfo clone() {
+			return this;
+		}
+		
+		public String getSrcFile() {
+			return source;
+		}
+
+		public int getSrcLine() {
+			return beginSrcLine;
+		}
+
+		public int getBeginSrcLine() {
+			return beginSrcLine;
+		}
+
+		public int getEndSrcLine() {
+			return endSrcLine;
+		}
+
+		public  String toString() {
+			return (source == null ? "nofile" : source)
+				+ (beginSrcLine >= 0 ? ":"+beginSrcLine : "");
+		}
+		
+		void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			// throw new NotImplementedException();
+			return null;
+		}
+	}
+}
